dbda1f4cb3f35f5be7b962a441b4109417702d65
Priti <kumaripriti.0609@gmail.com>
2023-04-24 18:03:29 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const qrcode = require('qrcode-terminal');
+
+const { Client } = require('whatsapp-web.js');
+const client = new Client();
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', message => {
+	if(message.body === 'hey') {
+		message.reply('hello');
+	}
+});
+
+
+client.initialize();
