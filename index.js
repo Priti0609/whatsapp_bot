eae10e1857e6ad9b76f13a458c2524e26698bcec
Priti <kumaripriti.0609@gmail.com>
2023-05-02 21:06:50 +0530

    added localauth

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const qrcode = require('qrcode-terminal');
 
-const { Client } = require('whatsapp-web.js');
-const client = new Client();
+const { Client,LocalAuth } = require('whatsapp-web.js');
+
+const client = new Client({
+	authStrategy: new LocalAuth()
+});
 
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
@@ -9,13 +12,28 @@ client.on('qr', qr => {
 
 client.on('ready', () => {
     console.log('Client is ready!');
+
+	//Number where you want to send the message
+	const number="+919123161217";
+
+	//Your message.
+	const text="Hey this is Priti";
+    
+	//Getting chatid from the number.
+	//we have to delete "+";
+
+	const chatid= number.substring(1)+"@c.us";
+
+	//sending message.
+	client.sendMessage(chatid,text);
+
 });
 
-client.on('message', message => {
-	if(message.body === 'hey') {
-		message.reply('hello');
+/*client.on('message', message => {
+	if(message.body === '!ping') {
+		message.reply('pong');
 	}
-});
+});*/
 
 
 client.initialize();
